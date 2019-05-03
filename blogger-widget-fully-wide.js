56ea9440be24cc355a9793b4b22cfafb54ee3219
Kawsik Roy <5407202+KawsikRoy@users.noreply.github.com>
2019-05-04 01:24:20 +0600

    Create blogger-widget-fully-wide.js

---
+++ blogger-widget-fully-wide.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('div.page_body div.widget.HTML h3').hide();
+  $('div.page_body div.widget.HTML').width($(window).width());
+  $('div.page_body div.widget.HTML').css( 'margin-left' , (-parseFloat($('.page_body .centered').css('margin-left')) - parseFloat($('#main').css('padding-left'))));
+  $(window).resize(function() {
+    $('div.page_body div.widget.HTML').width($(window).width());
+    $('div.page_body div.widget.HTML').css( 'margin-left' , (-parseFloat($('.page_body .centered').css('margin-left')) - parseFloat($('#main').css('padding-left'))));
+  });
+});
+</script>
+<script>
+$(document).ready(function(){
+  $(".hey").owlCarousel({
+items:1,
+loop: true,
+autoplay: true
+});
+});
